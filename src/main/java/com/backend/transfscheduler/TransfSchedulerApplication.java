4190f3ea98f52a5029c9e1f0d3c0ad65d14ddcf1
gustavo-moliveira <gustavo.moliveira01@gmail.com>
2024-01-19 17:29:39 -0300

    feat: base project

---
+++ src/main/java/com/backend/transfscheduler/TransfSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.backend.transfscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransfSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransfSchedulerApplication.class, args);
+    }
+
+}
